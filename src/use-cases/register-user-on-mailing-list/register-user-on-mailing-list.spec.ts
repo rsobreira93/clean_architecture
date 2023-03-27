bdaf3d87146e9a50dfba9341489639024203ba0f
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-03-27 14:28:26 -0300

    feat: user data interface

---
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,16 @@
+import { UserData } from './dtos/user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user withe complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const usersRepository = new InMemoryUsersRepository(users)
+    // const registerUserOnMailingList = new RegisteruserOnMailingList(usersRepository)
+    // const name = 'any_name'
+    // const email = 'any_email@mail.com'
+    // const response = await registerUserOnMailingList.execute({ name, email })
+    // const user = await usersRepository.findUserByEmail(email)
+
+    // expect(user.name).toBe('any_name')
+  })
+})
