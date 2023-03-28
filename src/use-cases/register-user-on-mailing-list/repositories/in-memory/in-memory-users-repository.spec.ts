152dab68832dc6d9fa63404f09335f651af48a81
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-03-28 06:35:13 -0300

    test: add users repository

--- src/use-cases/register-user-on-mailing-list/repositories/in-memory/in-memory-users-repository.spec.ts
+++ src/use-cases/register-user-on-mailing-list/repositories/in-memory/in-memory-users-repository.spec.ts
@@ -1,12 +1,29 @@
+import { InMemoryUserRepository } from './in-memory-users-repository'
 import { UserData } from '../../dtos/user-data'
-import { InMemoryUsersRepository } from './in-memory-users-repository'
 
 describe('In memory User repository', () => {
   test('should return null if user is not found', async () => {
     const users: UserData[] = []
-    const userRepo = new InMemoryUsersRepository(users)
-    const user = await userRepo.findUserByEmail('any@mail.com')
-
+    const sut = new InMemoryUserRepository(users)
+    const user = await sut.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
+  })
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@mail.com'
+    const sut = new InMemoryUserRepository(users)
+    await sut.create({ name, email })
+    const user = await sut.findUserByEmail('any@mail.com')
+    expect(user.name).toBe('any_name')
+  })
+
+  test('should return all users in the repository', async () => {
+    const users: UserData[] = [{ name: 'any_name', email: 'any@mail.com' },
+      { name: 'second_name', email: 'second@mail.com' }]
+    const sut = new InMemoryUserRepository(users)
+    const returnedUsers = sut.findAllUsers()
+    expect((await returnedUsers).length).toBe(2)
   })
 })
