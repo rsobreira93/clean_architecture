2256708d33e3840c6226d05f44b8bddb887bdb34
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-03-29 07:08:18 -0300

    feat: add validadtion email and local part

---
+++ src/entities/email.ts
@@ -0,0 +1,19 @@
+export class Email {
+  static validate (email:string):boolean {
+    if (!email) {
+      return false
+    }
+
+    if (email.length > 320) {
+      return false
+    }
+
+    const [local] = email.split('@')
+
+    if (local.length > 64) {
+      return false
+    }
+
+    return true
+  }
+}
