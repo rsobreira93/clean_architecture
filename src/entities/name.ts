2d7e2a6761b9b0aaaec9797216bf4e8d244857ce
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-04-07 11:17:56 -0300

    style adjust {}

--- src/entities/name.ts
+++ src/entities/name.ts
@@ -17,9 +17,8 @@ export class Name {
   }
 
   public static validate (name:string): boolean {
-    if (!name) return false
-
-    if (name.trim().length < 2 || name.trim().length > 256) return false
+    if (!name) { return false }
+    if (name.trim().length < 2 || name.trim().length > 256) { return false }
 
     return true
   }
