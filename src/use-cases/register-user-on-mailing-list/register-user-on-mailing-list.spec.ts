54f37db43a56b6c38ac0fed201ba48ff49dd3a00
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-04-18 09:18:43 -0300

    test: regisger user on mailing list and dont add on invalid email

--- src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,16 +1,33 @@
-import { UserData } from './dtos/user-data'
+import { InvalidEmailError } from '../../entities/errors/invalid-email-errors'
+import { UserData } from '../../entities/user-data'
+import { left } from '../../shared'
+import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
+import { InMemoryUserRepository } from './repositories/in-memory/in-memory-users-repository'
 
 describe('Register user on mailing list use case', () => {
   test('should add user withe complete data to mailing list', async () => {
     const users: UserData[] = []
-    console.log(users)
-    // const usersRepository = new InMemoryUsersRepository(users)
-    // const registerUserOnMailingList = new RegisteruserOnMailingList(usersRepository)
-    // const name = 'any_name'
-    // const email = 'any_email@mail.com'
-    // const response = await registerUserOnMailingList.execute({ name, email })
-    // const user = await usersRepository.findUserByEmail(email)
+    const usersRepository = new InMemoryUserRepository(users)
+    const registerUserOnMailingList = new RegisterUserOnMailingList(usersRepository)
+    const name = 'any_name'
+    const email = 'any_email@mail.com'
+    const response = await registerUserOnMailingList.execute({ name, email })
+    const user = await usersRepository.findUserByEmail(email)
 
-    // expect(user.name).toBe('any_name')
+    expect(user.name).toBe('any_name')
+    expect(response.value.name).toBe('any_name')
+  })
+
+  test('should not be able to add user with invalid email to mailing list', async () => {
+    const users: UserData[] = []
+    const usersRepository = new InMemoryUserRepository(users)
+    const registerUserOnMailingList = new RegisterUserOnMailingList(usersRepository)
+    const name = 'any_name'
+    const invalidEmail = 'invalid_email'
+    const response = await registerUserOnMailingList.execute({ name, email: invalidEmail })
+    const user = await usersRepository.findUserByEmail(invalidEmail)
+
+    expect(user).toBeNull()
+    expect(response).toEqual(left(new InvalidEmailError()))
   })
 })
