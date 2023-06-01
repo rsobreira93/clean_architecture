fa7468e7dd47c1f4bb51d964f2856dac69ed09e3
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-06-01 07:24:34 -0300

    feat: web controller should return 500 when server rises

--- src/web-controllers/register-use-controller.ts
+++ src/web-controllers/register-use-controller.ts
@@ -1,32 +1,36 @@
-import { RegisterUserOnMailingList } from '@/use-cases/register-user-on-mailing-list/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from './interfaces'
 import { UserData } from '@/entities'
-import { badRequest, created } from './util'
+import { badRequest, created, serverError } from './util'
 import { MissingParamError } from './errors/missing-param-error'
+import { UseCase } from '@/use-cases/interfaces'
 
 export class RegisterUserController {
-  private readonly useCase: RegisterUserOnMailingList
-  constructor (useCase: RegisterUserOnMailingList) {
+  private readonly useCase: UseCase
+  constructor (useCase: UseCase) {
     this.useCase = useCase
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const userData: UserData = request.body
-    const response = await this.useCase.execute(userData)
+    try {
+      const userData: UserData = request.body
+      const response = await this.useCase.perform(userData)
 
-    if (!request.body.name || !request.body.email) {
-      let missingParam = !(request.body.name) ? 'name' : ''
-      missingParam += !(request.body.email) ? 'email' : ''
+      if (!request.body.name || !request.body.email) {
+        let missingParam = !(request.body.name) ? 'name' : ''
+        missingParam += !(request.body.email) ? 'email' : ''
 
-      return badRequest(new MissingParamError(missingParam))
-    }
+        return badRequest(new MissingParamError(missingParam))
+      }
 
-    if (response.isLeft()) {
-      return badRequest(response.value)
-    }
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
 
-    if (response.isRight()) {
-      return created(response.value)
+      if (response.isRight()) {
+        return created(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
