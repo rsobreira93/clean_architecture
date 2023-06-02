9096e4f45a681bead0a1ac56b516405df691a3f4
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-06-02 06:53:15 -0300

    test: apply body parsing middleware with json

---
+++ test/main/middleware/body-parse.spec.ts
@@ -0,0 +1,15 @@
+import app from '@/main/config/app'
+import request from 'supertest'
+
+describe('Body parser middleware', () => {
+  test('should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'John' })
+      .expect({ name: 'John' })
+  })
+})
