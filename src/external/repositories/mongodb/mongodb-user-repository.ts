64a922d3ab54479a19c489c6ba0aa1e6dbeb4483
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-06-14 15:12:48 -0300

    feat: implement mongodb users repository

---
+++ src/external/repositories/mongodb/mongodb-user-repository.ts
@@ -0,0 +1,32 @@
+import { UserData } from '@/entities'
+import { UsersRepository } from '@/use-cases/register-user-on-mailing-list/repositories/users-repository'
+import { mongoHelper } from './helper'
+
+export class MongodbUserRepository implements UsersRepository {
+  async create (user: UserData): Promise<void> {
+    const userCollection = mongoHelper.getCollection('users')
+    const exists = await this.exists(user)
+
+    if (!exists) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const userCollection = mongoHelper.getCollection('users')
+
+    return await userCollection.findOne({ email })
+  }
+
+  findAllUsers (): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email)
+    if (result !== null) {
+      return true
+    }
+    return false
+  }
+}
