5ffd585a13bc2592da00b293d50e29106f17b71a
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-06-14 13:25:45 -0300

    feat: mongodb helper

---
+++ src/external/repositories/mongodb/helper/mongo-helper.ts
@@ -0,0 +1,24 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const mongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  clearCollection (name: string): void {
+    return this.getCollection(name).deleteMany({})
+  }
+
+}
