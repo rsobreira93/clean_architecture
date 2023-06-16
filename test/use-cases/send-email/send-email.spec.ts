32bc361dfc5d66428d2fbff1370f0c5ad188885e
Romulo Sobreira <sobreiraromuloes@gmail.com>
2023-06-16 06:49:24 -0300

    chore: add example attachment file

---
+++ test/use-cases/send-email/send-email.spec.ts
@@ -0,0 +1,12 @@
+const attachmentFilePath = '../resources/text.txt'
+const fromName = 'Test'
+const fromEmail = 'test@example.com'
+const toName = 'any_name'
+const toEmail = 'any_email@example.com'
+const subject = 'Test email'
+const emailBody = 'Hello world attachment test'
+const emailBodyHtml = '<b>Hello world attachment test</b>'
+const attachment = [{
+  filename: attachmentFilePath,
+  contentType: 'text/plain'
+}]
